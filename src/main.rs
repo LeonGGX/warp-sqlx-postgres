0b921975736559e2e19b95697a654a4274f68213
LeonGGX <37880486+LeonGGX@users.noreply.github.com>
2020-07-31 15:40:39 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ async fn main() {
     // and sets the constructed `Subscriber` as the default.
     tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
 
-    let db_url = "postgres://postgres:1922leon@localhost/persons";
+    let db_url = "postgres://postgres:password@localhost/persons";
     let pool = db::create_pg_pool(db_url).await.unwrap();
 
     let api = filters::person_filters(pool);
